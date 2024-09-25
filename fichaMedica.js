d4435e7d6d1407876bb3264b954ccfd891e4080f
MatheusLombard <matheus.profissional2022@gmail.com>
2024-09-25 19:26:52 -0300

    Update fichaMedica.js

--- fichaMedica.js
+++ fichaMedica.js
@@ -1,5 +1,5 @@
 const Sequelize  = require("sequelize");
-const database = require('../conexao')
+const database = require('./conexao')
 // const Usuarios = require('./usuarios')
 
 const FichaMedica = database.define('FichaMedica', {
