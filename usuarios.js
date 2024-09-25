1ae4c1e62062986047f723e843df7b816d98925a
MatheusLombard <matheus.profissional2022@gmail.com>
2024-09-25 19:27:41 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,5 +1,5 @@
 const Sequelize  = require("sequelize");
-const database = require('../conexao');
+const database = require('./conexao');
 const FichaMedica = require("./fichaMedica");
 const Doenca = require("./doenca");
 const UsuarioDoenca = require("./usuarioDoenca");
